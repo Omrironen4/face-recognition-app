13846e91a1966ae466069a9534bfff2020843e9b
Omri <omri.ronen4@gmail.com>
2022-02-25 19:26:29 -0800

    added brain logo with tilty effect

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Tilty from 'react-tilty';
+import './Logo.css'
+import brain from './brain.png'
+
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-
+            <Tilty className="tilty"  style={{ scale: 1.05 }}>
+                <div className="inner"><img alt='brain' src={brain}/></div>
+            </Tilty>
         </div>
     );
 }
