81eea512295132aead6bd4f6a721784d54d2e17c
Omri <omri.ronen4@gmail.com>
2022-03-02 14:39:54 -0800

    made minor changes to facerecognition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,9 +4,9 @@ import Box from '@mui/material/Box';
 
 const FaceRecognition = () => {
     return (
-        <Box className='center'>
-            <img alt='tree' src={'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'}/>
-        </Box>
+        <div className='center ma'>
+            <img alt='tree' src={'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'} width='50%'/>
+        </div>
     );
 }
 
