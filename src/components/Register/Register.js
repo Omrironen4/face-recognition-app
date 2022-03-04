e3d27d7d95823403b5e2c5eed82e836d6ef9e842
Omri <omri.ronen4@gmail.com>
2022-03-03 16:33:11 -0800

    added css to logout button

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,7 +6,7 @@ const Register = ({onRouteChange}) => {
         <div>
         <article className="br3 ba  b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f2 fw6 ph0 mh0">Register</legend>
                         <div className="mt3">
@@ -15,11 +15,11 @@ const Register = ({onRouteChange}) => {
                         </div>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-blue hover-black w-100" type="email" name="email-address" id="email-address" />
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-blue hover-black w-100" type="email" name="email-address" autoComplete='current-email' id="email-address" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-blue hover-black w-100" type="password" name="password" id="password" />
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-blue hover-black w-100" type="password" name="password" autoComplete='current-password' id="password" />
                         </div>
                 
                     </fieldset>
@@ -29,7 +29,7 @@ const Register = ({onRouteChange}) => {
                          className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib hover-bg-blue" type="submit"
                          value="Register" />
                     </div>
-                </form>
+                </div>
             </main>
         </article>
         </div>
